a5f69db3f0f474c72ab18235936d757e4699ca09
SGeorges <SGeorges@github.com>
2022-03-22 22:43:54 -0400

    Attempt at fixing utils test for system.in

--- src/test/java/com/georges/utils/ScannerUtilTest.java
+++ src/test/java/com/georges/utils/ScannerUtilTest.java
@@ -16,6 +16,7 @@ public class ScannerUtilTest {
 		ByteArrayInputStream in = new ByteArrayInputStream("TEST".getBytes());
 		System.setIn(in);
 		
+		System.out.println(scanner.getStringInput());
 		assertEquals("TRUE", scanner.getStringInput());
 	}
 	
